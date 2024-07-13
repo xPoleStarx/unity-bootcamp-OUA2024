c81370ac76ba274cc567aee257b44aaca73017cd
Seyfullah Korkmaz <seyfullahkorkmaz115@gmail.com>
2024-07-13 11:15:57 +0300

    new scene and characters added

---
+++ Lost Light/Assets/Scripts/State Machine/IdleState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    public void EnterState(PlayerController player)
+    {
+        Debug.Log("Enter Idle state");
+    }
+    public void ExitState(PlayerController player)
+    {
+        Debug.Log("Exit Idle state");
+    }
+
+    public void UpdateState(PlayerController player)
+    {
+        Debug.Log("Update Idle state");
+    }
+
+}
